f33dcde2ec6d2343f98d17a564541bad50a70ea0
KingVictoria <TheRealKingVictoria@gmail.com>
2018-03-08 18:51:20 -0500

    see last

--- Caravan/src/main/Caravan.java
+++ Caravan/src/main/Caravan.java
@@ -8,8 +8,20 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class Caravan extends JavaPlugin {
 	
 	/* TODO:
+	 * ==Phase One==
 	 * - Create "Shop" which stores one collection chest and one distribution chest
-	 * - 
+	 * - Create ReceiptChest extension of CollectionChest with methods related to that
+	 * - Set up rudimentary test which sends items from Shop to ReceiptChest
+	 * ==Phase Two==
+	 * - Create TradeStation and Terminal
+	 * - Create Trade Lanes
+	 * - Create UI and Search Function
+	 * - Create Receipt Generation
+	 * - Set up test
+	 * ==Phase Three==
+	 * - Add costs for everything
+	 * - Create settings document to easily toy with values and costs
+	 * - Bug test for glitches
 	 */
 	
 	/**
